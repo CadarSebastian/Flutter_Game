8dec3715188238b316a214739689c31a65767bbf
CadarSebastian <sebicadar03@gmail.com>
2024-01-29 15:38:29 +0200

    making base home page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -9,18 +10,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Expanded(
-          child: Container(
-            color: Colors.black,
+        Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.red[800],
+              ),
+            ),
+          ],
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: ElevatedButton(
+            onPressed: () {
+              // Add your logic to start the game
+              print('Game started!');
+            },
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(20.0),
+               primary: const Color.fromARGB(255, 104, 2, 18), 
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Icon(
+      Icons.play_arrow, // Replace this with the desired icon
+      size: 40.0, // Adjust the size of the icon as needed
+                
+              ),
+            ),
           ),
-          ),
-          Expanded(
-          child: Container(
-            color: Colors.red[800],
-          ),
-          ),
+        ),
       ],
     );
   }
